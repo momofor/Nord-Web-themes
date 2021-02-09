bbbeffd274f055356fc88b98e4c1bfb28c75193d
Ryan The Ghost <78041469+Ryan-da-ghost@users.noreply.github.com>
2021-02-09 11:14:48 +0330

    Create urlCheck.js

---
+++ javascript/urlCheck.js
@@ -0,0 +1,85 @@
+"use strict";
+let thisSite = window.location.host;
+
+// Check the current URL
+if (thisSite === "discordapp.com" || thisSite === "discord.com") {
+  doDiscord();
+} else if (thisSite === "www.youtube.com") {
+  doYouTube();
+} else if(thisSite === "www.deviantart.com") {
+  doDev();
+} else if(thisSite === "monkeytype.com") {
+  doMonkey();
+} else if(thisSite === "www.reddit.com") {
+  doReddit();
+}
+
+
+// ---------------------------- AD CSS ------------------------------
+
+// discord.com
+function doDiscord() {
+  let a = chrome.extension.getURL("css/discord.css"),
+    link = document.createElement("link");
+
+  link.type = "text/css";
+  link.rel = "stylesheet";
+  link.id = "webThemesFramework";
+  link.href = a;
+
+  document.head.appendChild(link);
+}
+
+// www.youtube.com
+function doYouTube() {
+  let a = chrome.extension.getURL("css/youtube.css"),
+    link = document.createElement("link");
+
+  link.type = "text/css";
+  link.rel = "stylesheet";
+  link.id = "webThemesFramework";
+  link.href = a;
+
+  document.head.appendChild(link);
+}
+
+// www.deviantart.com
+function doDev() {
+
+  let a = chrome.extension.getURL("css/deviantart.css"),
+    link = document.createElement("link");
+
+  link.type = "text/css";
+  link.rel = "stylesheet";
+  link.id = "webThemesFramework";
+  link.href = a;
+
+  document.head.appendChild(link);
+}
+
+// monkeytype.com
+function doMonkey() {
+
+  let a = chrome.extension.getURL("css/monkeytype.css"),
+    link = document.createElement("link");
+
+  link.type = "text/css";
+  link.rel = "stylesheet";
+  link.id = "webThemesFramework";
+  link.href = a;
+
+  document.head.appendChild(link);
+}
+
+// www.reddit.com
+function doReddit() {
+  let a = chrome.extension.getURL("css/reddit.css"),
+    link = document.createElement("link");
+
+  link.type = "text/css";
+  link.rel = "stylesheet";
+  link.id = "webThemesFramework";
+  link.href = a;
+
+  document.head.appendChild(link);
+}
