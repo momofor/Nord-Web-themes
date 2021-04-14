8849987de4e524e599c7f4e5d44a1cafe6cfd7b3
momofor <mohammedclacher2014@gmail.com>
2021-04-14 00:35:59 +0000

    refactored javascript

--- javascript/urlCheck.js
+++ javascript/urlCheck.js
@@ -1,50 +1,39 @@
 "use strict";
 let thisSite = window.location.host;
 
-// Check the current URL
-if (thisSite === "discordapp.com" || thisSite === "discord.com") {
-  doDiscord();
-} else if (thisSite === "www.youtube.com") {
-  doYouTube();
-} else if (thisSite === "github.com") {
-  doDev();
+const websites = {
+	discord: "discordapp.com" || "discord.com",
+	youtube: "www.youtube.com",
+	deviantArt: "www.deviantart.com",
+	monkeyType: "monkeytype.com",
+	reddit: "www.reddit.com",
+};
+for (let i = 0; i < websites.length; i++) {
+	curWebsite = websites[i];
+	console.log(curWebsite);
+}
+switch (thisSite) {
+	case websites.discord:
+		doWebsite("discord");
+		break;
+	case websites.youtube:
+		doWebsite("youtube");
+		break;
+	case websites.deviantArt:
+		doWebsite("deviantart");
+		break;
+	case websites.monkeyType:
+		doWebsite("monkeytype");
+		break;
+	case websites.reddit:
+		doWebsite("reddit");
+		break;
 }
 
-// discord.com
-function doDiscord() {
-  let a = chrome.extension.getURL("css/discord.css"),
-    link = document.createElement("link");
-
-  link.type = "text/css";
-  link.rel = "stylesheet";
-  link.id = "Nord Web Themes";
-  link.href = a;
-
-  document.head.appendChild(link);
-}
-
-// www.youtube.com
-function doYouTube() {
-  let a = chrome.extension.getURL("css/youtube.css"),
-    link = document.createElement("link");
-
-  link.type = "text/css";
-  link.rel = "stylesheet";
-  link.id = "Nord Web Themes";
-  link.href = a;
-
-  document.head.appendChild(link);
-}
-
-// github.com
-function doDev() {
-  let a = chrome.extension.getURL("css/github.css"),
-    link = document.createElement("link");
-
-  link.type = "text/css";
-  link.rel = "stylesheet";
-  link.id = "Nord Web Themes";
-  link.href = a;
-
-  document.head.appendChild(link);
-}
+// ---------------------------- ADD CSS ------------------------------
+const doWebsite = (name) => {
+	let a = chrome.extension.getURL(`css/${name}.css`);
+	link = document.createElement("link");
+	link.innerHTML = `rel = "stylesheet" href=${a} id = "Nord websites"`;
+	document.head.appendChild(link);
+};
