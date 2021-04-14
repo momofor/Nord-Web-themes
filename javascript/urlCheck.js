2343673673cc73a731dc04a7e3dcda49f4d0a667
momofor <mohammedclacher2014@gmail.com>
2021-04-14 00:21:32 +0000

    Refactor urlcheck.js

--- javascript/urlCheck.js
+++ javascript/urlCheck.js
@@ -1,85 +1,39 @@
 "use strict";
 let thisSite = window.location.host;
 
-// Check the current URL
-if (thisSite === "discordapp.com" || thisSite === "discord.com") {
-  doDiscord();
-} else if (thisSite === "www.youtube.com") {
-  doYouTube();
-} else if(thisSite === "www.deviantart.com") {
-  doDev();
-} else if(thisSite === "monkeytype.com") {
-  doMonkey();
-} else if(thisSite === "www.reddit.com") {
-  doReddit(); // uncomment this
+const websites = {
+	discord: "discordapp.com" || "discord.com",
+	youtube: "www.youtube.com",
+	deviantArt: "www.deviantart.com",
+	monkeyType: "monkeytype.com",
+	reddit: "www.reddit.com",
+};
+for (let i = 0; i < websites.length; i++) {
+	curWebsite = websites[i];
+	console.log(curWebsite);
 }
-
+switch (thisSite) {
+	case websites.discord:
+		doWebsite("discord");
+		break;
+	case websites.youtube:
+		doWebsite("youtube");
+		break;
+	case websites.deviantArt:
+		doWebsite("deviantart");
+		break;
+	case websites.monkeyType:
+		doWebsite("monkeytype");
+		break;
+	case websites.reddit:
+		doWebsite("reddit");
+		break;
+}
 
 // ---------------------------- ADD CSS ------------------------------
-
-// discord.com
-function doDiscord() {
-  let a = chrome.extension.getURL("css/discord.css"),
-    link = document.createElement("link");
-
-  link.type = "text/css";
-  link.rel = "stylesheet";
-  link.id = "Nord websites";
-  link.href = a;
-
-  document.head.appendChild(link);
-}
-
-// www.youtube.com
-function doYouTube() {
-  let a = chrome.extension.getURL("css/youtube.css"),
-    link = document.createElement("link");
-
-  link.type = "text/css";
-  link.rel = "stylesheet";
-  link.id = "Nord websites";
-  link.href = a;
-
-  document.head.appendChild(link);
-}
-
-// www.deviantart.com
-function doDev() {
-
-  let a = chrome.extension.getURL("css/deviantart.css"),
-    link = document.createElement("link");
-
-  link.type = "text/css";
-  link.rel = "stylesheet";
-  link.id = "Nord websites";
-  link.href = a;
-
-  document.head.appendChild(link);
-}
-
-// monkeytype.com
-function doMonkey() {
-
-  let a = chrome.extension.getURL("css/monkeytype.css"),
-    link = document.createElement("link");
-
-  link.type = "text/css";
-  link.rel = "stylesheet";
-  link.id = "Nord websites";
-  link.href = a;
-
-  document.head.appendChild(link);
-}
-
-// www.reddit.com
-function doReddit() {
-  let a = chrome.extension.getURL("css/reddit.css"),
-    link = document.createElement("link");
-
-  link.type = "text/css";
-  link.rel = "stylesheet";
-  link.id = "Nord websites";
-  link.href = a;
-
-  document.head.appendChild(link);
-}
+const doWebsite = (name) => {
+	let a = chrome.extension.getURL(`css/${name}.css`);
+	link = document.createElement("link");
+	link.innerHTML = `rel = "stylesheet" href=${a} id = "Nord websites"`;
+	document.head.appendChild(link);
+};
